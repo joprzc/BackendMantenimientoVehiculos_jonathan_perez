2c41c59a14d8afbd6ab628ae0cbe6ea86eebe3a1
jonathan perez <jonathan.przc@gmail.com>
2026-04-22 17:02:16 -0500

    Configurar variables de entorno para desplegar el proyecto: gitHub-neon-render

--- registro/urls.py
+++ registro/urls.py
@@ -66,6 +66,6 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    urlpatterns += static[
+    urlpatterns += [
         re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     ]
